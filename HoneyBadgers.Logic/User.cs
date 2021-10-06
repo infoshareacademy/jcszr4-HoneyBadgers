bc85aed3124bc1437d147a6bf0ed2e6af76f7632
Beata Gielbaga <bgielbaga@gmail.com>
2021-10-06 18:08:02 +0200

    #jcszr4hb - dodawanie wczytywania danych z pliku + inicjalizowanie początkowych danych

---
+++ HoneyBadgers.Logic/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyBadgers.Logic
+{
+    public class User
+    {
+        public string Id;
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public List<Movie> Movies;
+    }
+}
