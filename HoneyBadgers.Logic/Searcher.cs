9d907c205ac9fa4b21e745c747875e7e04441bb2
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2021-10-09 19:46:29 +0200

    Add value to search results and sorting by value (higher value to lower value)

--- HoneyBadgers.Logic/Searcher.cs
+++ HoneyBadgers.Logic/Searcher.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HoneyBadgers.Logic
 {
     public static class Searcher
     {
-        public static Dictionary<Movie,int> FindByName(List<Movie> db, string searchInput)
+        public static Dictionary<Movie,int> FindByName(Data db, string searchInput)
         {
-            var partsInput = searchInput.Split(" "); 
+            var inputParts = searchInput.Split(" "); 
             var results = new Dictionary<Movie, int>();
-            foreach (var itemDB in db)
+            foreach (var itemDB in db.Movies)
             {
-                var movieTitle = itemDB.Title;
-                var searchMatched = 0;
-                foreach (var partInput in partsInput)
+                var movieTitle = itemDB.Title.ToLower();
+                var searchMatched = 0; //TODO: Dobrać odpowiednią nazwę!!
+                foreach (var partInput in inputParts)
                 {
-                    if (movieTitle.Contains(partInput))
+                    if (movieTitle.Contains(partInput.ToLower()))
                     {
                         searchMatched += 1;
                     }
@@ -22,9 +26,12 @@ namespace HoneyBadgers.Logic
                 
                 if (searchMatched > 0)
                 {
+                    
                     results.Add(itemDB,searchMatched);
-                }
+                } 
             }
+
+            results.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
             return results;
         }
     }
