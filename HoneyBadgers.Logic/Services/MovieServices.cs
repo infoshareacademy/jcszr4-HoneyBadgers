49d3ea324ebcf2d8024d8cbc6fa846cf6af69577
Beata Gielbaga <bgielbaga@gmail.com>
2021-11-08 21:15:28 +0100

    Dodanie logo

--- HoneyBadgers.Logic/Services/MovieServices.cs
+++ HoneyBadgers.Logic/Services/MovieServices.cs
@@ -5,5 +5,7 @@ namespace HoneyBadgers.Logic.Services
     class MovieServices
     {
         //TODO:Czy nie lepiej były jak repositorium oznaczało a'la baze danych? A services odpowiedał zaobsługę? do przemyślenia
+        // B: Jak dla mnie skoro wszystko jest w repo to nie ma po co tworzyć Service
+
     }
 }
