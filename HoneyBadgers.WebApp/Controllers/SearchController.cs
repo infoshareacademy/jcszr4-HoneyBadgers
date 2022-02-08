b895d0280923cee542b2483fa0d784427cf63d1c
Sebastian Leszczyński <sebastian.leszczynski100@gmail.com>
2022-02-08 20:25:22 +0100

    Configuring SearchController

--- HoneyBadgers.WebApp/Controllers/SearchController.cs
+++ HoneyBadgers.WebApp/Controllers/SearchController.cs
@@ -1,14 +1,38 @@
-﻿using HoneyBadgers.Entity.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HoneyBadgers.Entity.Models;
+using HoneyBadgers.Entity.Repositories;
+using HoneyBadgers.Logic.Models;
+using HoneyBadgers.Logic.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HoneyBadgers.WebApp.Controllers
 {
     public class SearchController : Controller
     {
+        private readonly IRepository<Movie> _movieRepository;
+
+        public SearchController(IRepository<Movie> movieRepository)
+        {
+            _movieRepository = movieRepository;
+        }
         public IActionResult Index(string search)
         {
-            var model = new Search();
-            model.Query = search;
+            List<Movie> model;
+
+            if (search == null)
+            {
+                model = _movieRepository
+                    .GetAll()
+                    .ToList();
+                return View(model);
+            }
+
+            model = _movieRepository
+                .GetAll()
+                .Where(movie => movie.Title.ToLower().Contains(search))
+                .ToList();
             return View(model);
         }
     }
