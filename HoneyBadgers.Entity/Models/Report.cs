fed87ba5e662f9908d19ce7939751613c09c73fc
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2022-02-20 17:37:23 +0100

    Usunięcie metody testowej

--- HoneyBadgers.Entity/Models/Report.cs
+++ HoneyBadgers.Entity/Models/Report.cs
@@ -6,28 +6,5 @@ namespace HoneyBadgers.Entity.Models
     {
         public Guid Id { get; private set; }
         public string Body { get; set; }
-
-        public Report()
-        {
-            Id = Guid.NewGuid();
-            Body = GenerateString();
-        }
-
-        private static string GenerateString()
-        {
-            var res = new Random();
-            var allChars = "abcdefghijklmnopqrstuvwxyz0123456789";
-            var stringSize = 50;
-            var randomString = "";
-
-            for (var i = 0; i < stringSize; i++)
-            {
-                var x = res.Next(allChars.Length);
-                randomString += allChars[x];
-            }
-
-            return randomString;
-
-        }
     }
 }
