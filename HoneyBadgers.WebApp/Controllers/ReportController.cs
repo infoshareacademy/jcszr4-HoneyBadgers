aa6de11ea676b7cdc62085dbe0e02cd987d3dd24
Beata Gielbaga <bgielbaga@gmail.com>
2022-03-06 18:11:12 +0100

    #28 - dodanie chart.js

--- HoneyBadgers.WebApp/Controllers/ReportController.cs
+++ HoneyBadgers.WebApp/Controllers/ReportController.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Composition;
-using System.Diagnostics;
+using System.Data;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using HoneyBadgers.Entity.Models;
-using HoneyBadgers.WebApp.Models;
 using Newtonsoft.Json;
 
 namespace HoneyBadgers.WebApp.Controllers
@@ -39,5 +37,47 @@ namespace HoneyBadgers.WebApp.Controllers
             return View(json);
         }
 
+        [HttpGet]
+        public async Task<ActionResult> GenreReport()
+        {
+            var expenses = new List<Tuple<string, int>>();
+            expenses.Add(new Tuple<string, int>("Book", 3));
+            expenses.Add(new Tuple<string, int>("Horror", 8));
+            expenses.Add(new Tuple<string, int>("Fantasy", 6));
+            expenses.Add(new Tuple<string, int>("Bookf", 1));
+
+            ViewBag.Ex = expenses;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult NewChart()
+        {
+            var strEducation = "Book";
+            var educationValue = Math.Round(34.6, 0);
+
+            List<object> iData = new List<object>();
+            //Creating sample data    
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Expense", System.Type.GetType("System.String"));
+            dt.Columns.Add("ExpenseValues", System.Type.GetType("System.Int32"));
+
+            //For Education  
+            DataRow dr = dt.NewRow();
+            dr["Expense"] = strEducation;
+            dr["ExpenseValues"] = educationValue;
+            dt.Rows.Add(dr);
+
+            //Looping and extracting each DataColumn to List<Object>    
+            foreach (DataColumn dc in dt.Columns)
+            {
+                List<object> x = new List<object>();
+                x = (from DataRow drr in dt.Rows select drr[dc.ColumnName]).ToList();
+                iData.Add(x);
+            }
+            ViewBag.ChartData = iData;
+            //Source data returned as JSON    
+            return Json(iData);
+        }
     }
 }
