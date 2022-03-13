a1114147840da49a6587524bca3cca0c5df15909
Patryk Wisniewski <patryk.wisniewski@ge.com>
2022-03-13 11:34:05 +0100

    merge

---
+++ HoneyBadgers.WebApp/Models/ReportGenreStats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HoneyBadgers.WebApp.Models
+{
+    public class ReportGenreStats
+    {
+        public string GenreName { get; init; }
+        public int TimesInDB { get; init; }
+        public int RowsInDB { get; init; }
+        public double Precents { get; set; }
+    
+        public Tuple<string, int> ReportAsTuple => new Tuple<string, int>(GenreName, TimesInDB);
+
+        public string Body { get; set; }
+        public string Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+
+        public ReportGenreStats()
+        {
+            GenreName = "empty";
+            TimesInDB = 0;
+            RowsInDB = 0;
+            Body = $"GenreName: {GenreName}; TimesInDb: {TimesInDB}; RowsInDb: {RowsInDB}; Percent: {Precents}%; ReportAsTuple: {ReportAsTuple.Item1}{ReportAsTuple.Item2}";
+            Precents = 0.0;
+        }
+
+        public ReportGenreStats(string genreName, int trowsInDb, int rowsInDb)
+        {
+            GenreName = genreName;
+            TimesInDB = trowsInDb;
+            RowsInDB = rowsInDb;
+            Body = $"GenreName: {GenreName}; TimesInDb: {TimesInDB}; RowsInDb: {RowsInDB}; Percent: {Precents}%; ReportAsTuple: {ReportAsTuple.Item1}{ReportAsTuple.Item2}";
+            Precents = TimesInDB / RowsInDB * 100;
+
+        }
+    }
+}
