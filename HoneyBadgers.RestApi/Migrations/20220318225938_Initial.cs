6489db6bade79ba5f4710ab6755334625c607780
Patryk Wiśniewski <pwisniewski1991@gmail.com>
2022-03-20 16:59:18 +0100

    fix migration

--- HoneyBadgers.RestApi/Migrations/20220318225938_Initial.cs
+++ HoneyBadgers.RestApi/Migrations/20220318225938_Initial.cs
@@ -7,38 +7,38 @@ namespace HoneyBadgers.RestApi.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "GenreStats",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
-                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    UserEmail = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
-                    GenreId = table.Column<int>(type: "int", nullable: false),
-                    GenreName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
-                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_GenreStats", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "GenreStats",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
+            //        UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        UserEmail = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+            //        GenreId = table.Column<int>(type: "int", nullable: false),
+            //        GenreName = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+            //        CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_GenreStats", x => x.Id);
+            //    });
 
-            migrationBuilder.CreateTable(
-                name: "ReportGenreStatsModels",
-                columns: table => new
-                {
-                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
-                    GenreName = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    TimesInDB = table.Column<int>(type: "int", nullable: false),
-                    RowsInDB = table.Column<int>(type: "int", nullable: false),
-                    Precents = table.Column<double>(type: "float", nullable: false),
-                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
-                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_ReportGenreStatsModels", x => x.Id);
-                });
+            //migrationBuilder.CreateTable(
+            //    name: "ReportGenreStatsModels",
+            //    columns: table => new
+            //    {
+            //        Id = table.Column<string>(type: "nvarchar(450)", nullable: false, defaultValueSql: "NEWID()"),
+            //        GenreName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+            //        TimesInDB = table.Column<int>(type: "int", nullable: false),
+            //        RowsInDB = table.Column<int>(type: "int", nullable: false),
+            //        Precents = table.Column<double>(type: "float", nullable: false),
+            //        Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+            //        CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getdate()")
+            //    },
+            //    constraints: table =>
+            //    {
+            //        table.PrimaryKey("PK_ReportGenreStatsModels", x => x.Id);
+            //    });
 
             migrationBuilder.CreateTable(
                 name: "UserActivity",
@@ -57,17 +57,17 @@ namespace HoneyBadgers.RestApi.Migrations
                     table.PrimaryKey("PK_UserActivity", x => x.Id);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_GenreStats_Id",
-                table: "GenreStats",
-                column: "Id",
-                unique: true);
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_GenreStats_Id",
+            //    table: "GenreStats",
+            //    column: "Id",
+            //    unique: true);
 
-            migrationBuilder.CreateIndex(
-                name: "IX_ReportGenreStatsModels_Id",
-                table: "ReportGenreStatsModels",
-                column: "Id",
-                unique: true);
+            //migrationBuilder.CreateIndex(
+            //    name: "IX_ReportGenreStatsModels_Id",
+            //    table: "ReportGenreStatsModels",
+            //    column: "Id",
+            //    unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
